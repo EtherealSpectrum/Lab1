941e12c282157841b74a2f6eae326bbccff9daab
nikht <nikht@DESKTOP-GGQT434>
2025-10-26 12:00:06 +0200

    Test

---
+++ Lab2/src/Problem1AnotherThought.c
@@ -0,0 +1,30 @@
+/*
+ * Problem1.c
+ *
+ *  Created on: 24 Οκτ 2025
+ *      Author: Nikitas K.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TOTAL_INPUTS 3
+
+int main() {
+	int number, sumOfPositives = 0, sumOfNegatives = 0;
+	int i;
+	for (i = 0; i < TOTAL_INPUTS; i++) {
+		printf("Insert a number: ");
+		scanf("%d", &number);
+
+		if (number > 0)
+			sumOfPositives += number;
+		else
+			sumOfNegatives += number;
+	}
+	printf("The sum of positives is: %d\n", sumOfPositives);
+	printf("The average of all inputs is: %f",
+			(sumOfPositives + sumOfNegatives) / (double) TOTAL_INPUTS);
+
+	return EXIT_SUCCESS;
+}

41f6d0c866281e259d732dca3a7bf509fc3b740b
nikht <nikht@DESKTOP-GGQT434>
2025-11-03 04:44:47 +0200

    Added end cases for GCD in Problem 6.

--- Lab2/src/Problem6.c
+++ Lab2/src/Problem6.c
@@ -8,19 +8,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int gcd(unsigned int, unsigned int);
+unsigned int gcd(int, int);
 
 int main() {
-	printf("Insert number the first number: ");
-	int m;
+	printf("Insert 2 numbers separated with space: ");
+
+	int m, n;
 	scanf("%d", &m);
-
-	printf("Insert number the second number: ");
-	int n;
 	scanf("%d", &n);
-
-	if (m < 0) m = -m;
-	if (n < 0) n = -n;
 
 	printf("GCD(%d,%d) = %u", m, n, gcd(m, n));
 
@@ -30,8 +25,13 @@ int main() {
 /*
  *  return -1 only if it's not defined. GCD(0,0) it's not defined.
  */
-unsigned int gcd(unsigned int q, unsigned int d) {
-	if(q == 0 && d == 0) return -1
+unsigned int gcd(int q, int d) {
+
+	if(q == 0 && d == 0) return -1;
+
+	if (q < 0) q = -q;
+
+	if (d < 0) d = -d;
 
 	if (q == 0) return d;
 
